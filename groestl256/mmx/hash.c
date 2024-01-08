f04101a53a58ed2c12adb3ad87830e347ee3fa65
Fawad <153737+FawadHa1der@users.noreply.github.com>
2024-01-08 13:16:47 -0500

    update to random message

--- groestl256/mmx/hash.c
+++ groestl256/mmx/hash.c
@@ -523,7 +523,7 @@ int main(int argc, char **argv) {
     fread(hostData, sizeof(unsigned char), dataSize, file);
     fclose(file);
 
-    const char* message = "my message";
+    const char* message = "my message gdfjhghjkfdhgjklfdshgjklfdhgjkfdshkfjsdhgjfdlshgjkfdsghfjdklhgjfkdlghfjdkslhgfdjksgsdfhj    dsdscxcd3232322cc";
     size_t size = strlen(message);
 
     unsigned char* data = (unsigned char*)malloc(size );
